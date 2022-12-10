49e6d4f58528d07debc1e5ee3a2b7fb627a5eba1
Andrew Graham <andrew.s.graham15.civ@army.mil>
2022-12-10 12:21:17 -0600

    Completed Day8

--- 2022/Day8/solution.go
+++ 2022/Day8/solution.go
@@ -28,7 +28,7 @@ func main() {
 	lines, _ := readLines("data.txt")
 
 	var visible_trees = 0
-	//var max_scenic_score = 0
+	var max_scenic_score = 0
 	for idx, line := range lines {
 		// Top and bottom rows always visible
 		if idx == 0 || idx == len(lines)-1 {
@@ -43,7 +43,7 @@ func main() {
 					continue
 				}
 				var current_number, _ = strconv.Atoi(string(j))
-				//var scenic_score = 1
+				var scenic_score = 1
 				fmt.Println("Current number", current_number, "Row", idx+1, "Column", i+1)
 				// Definitely not an edge, let's check left
 				var visible_from_left = true
@@ -52,9 +52,16 @@ func main() {
 					// If a number to the left is too big, not visible this way
 					if left_number >= current_number {
 						fmt.Println("Not visible from left")
+						//fmt.Println("Left score:", i-x)
 						visible_from_left = false
+						scenic_score *= i - x
 						break
 					}
+				}
+				// Max scenic score possible
+				if visible_from_left {
+					//fmt.Println("Left score:", i)
+					scenic_score *= i
 				}
 
 				// Check right now
@@ -65,8 +72,16 @@ func main() {
 					if right_number >= current_number {
 						fmt.Println("Not visible from right")
 						visible_from_right = false
+						//fmt.Println("Right score:", x-i)
+						scenic_score *= x - i
 						break
 					}
+				}
+
+				// Max scenic score possible
+				if visible_from_right {
+					//fmt.Println("Right score:", len(line)-i-1)
+					scenic_score *= len(line) - i - 1
 				}
 
 				// Now to check for visibility up and down, which is more annoying
@@ -79,10 +94,17 @@ func main() {
 					if above_number >= current_number {
 						fmt.Println("Not visible from north")
 						visible_from_north = false
+						//fmt.Println("North score:", idx-x)
+						scenic_score *= idx - x
 						break
 					}
 				}
 
+				// Max scenic score possible
+				if visible_from_north {
+					//fmt.Println("North score:", idx)
+					scenic_score *= idx
+				}
 				var visible_from_south = true
 				for x := idx + 1; x < len(lines); x++ {
 					//fmt.Println("Row", x)
@@ -91,14 +113,28 @@ func main() {
 					if below_number >= current_number {
 						fmt.Println("Not visible from south")
 						visible_from_south = false
+						//fmt.Println("South score:", x-idx)
+						scenic_score *= x - idx
 						break
 					}
 				}
+				// Max scenic score possible
+				if visible_from_south {
+					//fmt.Println("South score:", len(lines)-idx-1)
+					scenic_score *= len(lines) - idx - 1
+				}
+
 				if visible_from_left || visible_from_right || visible_from_north || visible_from_south {
 					visible_trees += 1
 				}
+				if scenic_score > max_scenic_score {
+					max_scenic_score = scenic_score
+					//fmt.Println("Max score beaten!!")
+				}
+				//fmt.Println("Scenic score:", scenic_score)
 			}
 		}
 	}
 	fmt.Println(visible_trees)
+	fmt.Println(max_scenic_score)
 }
