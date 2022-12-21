9393986c94fae6affc7c427550b5c0797e7912bd
Andrew Graham <andrew.s.graham15.civ@army.mil>
2022-12-21 15:29:57 -0600

    Day9Sol2 complete

--- 2022/Day9/solution.go
+++ 2022/Day9/solution.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -29,7 +30,6 @@ func deduplicateSlice(input [][2]int) [][2]int {
 	var new_slice [][2]int
 	new_slice = append(new_slice, input[0])
 	for _, one_item := range input {
-		fmt.Println(one_item)
 		is_new := true
 		for _, k := range new_slice {
 			if one_item == k {
@@ -57,35 +57,52 @@ func moveHead(head [2]int, direction string) [2]int {
 	return head
 }
 
-func calculateFollowing(head [2]int, tail [2]int, direction string) [2]int {
-	if direction == "L" {
-		if tail[0]-head[0] > 1 {
-			//fmt.Println("Need to move tail LEFT from", tail)
-			tail[0] = head[0] + 1
-			tail[1] = head[1]
-			//fmt.Println("Tail moved to", tail, "to match", head)
-		}
-	} else if direction == "R" {
-		if head[0]-tail[0] > 1 {
-			// fmt.Println("Need to move tail RIGHT from", tail)
+func calculateFollowing(head [2]int, tail [2]int) [2]int {
+	// Need to account for complete diagonals
+	if math.Abs(float64(head[0]-tail[0])) > 1 && math.Abs(float64(head[1]-tail[1])) > 1 {
+		// fmt.Println("Hit pure diagonal.")
+		// fmt.Println("Comparing:", head, tail)
+		// Figure out which way to go diagonally
+		// Top right
+		if head[0] > tail[0] && head[1] > tail[1] {
 			tail[0] = head[0] - 1
-			tail[1] = head[1]
-			// fmt.Println("Tail moved to", tail, "to match", head)
-		}
-	} else if direction == "U" {
-		if head[1]-tail[1] > 1 {
-			// fmt.Println("Need to move tail UP from", tail)
 			tail[1] = head[1] - 1
-			tail[0] = head[0]
-			// fmt.Println("Tail moved to", tail, "to match", head)
-		}
-	} else if direction == "D" {
-		if tail[1]-head[1] > 1 {
-			// fmt.Println("Need to move tail DOWN from", tail)
+			//Bottom right
+		} else if head[0] > tail[0] && head[1] < tail[1] {
+			tail[0] = head[0] - 1
 			tail[1] = head[1] + 1
-			tail[0] = head[0]
-			// fmt.Println("Tail moved to", tail, "to match", head)
+			// Bottom left
+		} else if head[0] < tail[0] && head[1] < tail[1] {
+			tail[0] = head[0] + 1
+			tail[1] = head[1] + 1
+			// Top left
+		} else if head[0] < tail[0] && head[1] > tail[1] {
+			tail[0] = head[0] + 1
+			tail[1] = head[1] - 1
 		}
+		return tail
+	}
+	// Back to normal off-by-only-one statements
+	if tail[0]-head[0] > 1 && tail[0] > head[0] {
+		//fmt.Println("Need to move tail LEFT from", tail)
+		tail[0] = head[0] + 1
+		tail[1] = head[1]
+		//fmt.Println("Tail moved to", tail, "to match", head)
+	} else if head[0]-tail[0] > 1 && head[0] > tail[0] {
+		// fmt.Println("Need to move tail RIGHT from", tail)
+		tail[0] = head[0] - 1
+		tail[1] = head[1]
+		// fmt.Println("Tail moved to", tail, "to match", head)
+	} else if head[1]-tail[1] > 1 && head[1] > tail[1] {
+		// fmt.Println("Need to move tail UP from", tail)
+		tail[1] = head[1] - 1
+		tail[0] = head[0]
+		// fmt.Println("Tail moved to", tail, "to match", head)
+	} else if tail[1]-head[1] > 1 && tail[1] > head[1] {
+		// fmt.Println("Need to move tail DOWN from", tail)
+		tail[1] = head[1] + 1
+		tail[0] = head[0]
+		// fmt.Println("Tail moved to", tail, "to match", head)
 	}
 	return tail
 }
@@ -94,14 +111,14 @@ func main() {
 	lines, _ := readLines("data.txt")
 	// Assume 0,0 is starting point
 	head_position := [2]int{0, 0}
-	// one_position := [2]int{0, 0}
-	// two_position := [2]int{0, 0}
-	// three_position := [2]int{0, 0}
-	// four_position := [2]int{0, 0}
-	// five_position := [2]int{0, 0}
-	// six_position := [2]int{0, 0}
-	// seven_position := [2]int{0, 0}
-	// eight_position := [2]int{0, 0}
+	one_position := [2]int{0, 0}
+	two_position := [2]int{0, 0}
+	three_position := [2]int{0, 0}
+	four_position := [2]int{0, 0}
+	five_position := [2]int{0, 0}
+	six_position := [2]int{0, 0}
+	seven_position := [2]int{0, 0}
+	eight_position := [2]int{0, 0}
 	tail_position := [2]int{0, 0}
 	var tail_positions [][2]int
 	tail_positions = append(tail_positions, tail_position)
@@ -110,13 +127,29 @@ func main() {
 		direction := line_array[0]
 		amount, _ := strconv.Atoi(line_array[1])
 		for i := 0; i < amount; i++ {
+			// fmt.Println("Moving:", i)
 			// For Sol One
 			head_position = moveHead(head_position, direction)
-			tail_position = calculateFollowing(head_position, tail_position, direction)
+			// Added for part 2
+			one_position = calculateFollowing(head_position, one_position)
+			two_position = calculateFollowing(one_position, two_position)
+			three_position = calculateFollowing(two_position, three_position)
+			four_position = calculateFollowing(three_position, four_position)
+			five_position = calculateFollowing(four_position, five_position)
+			six_position = calculateFollowing(five_position, six_position)
+			seven_position = calculateFollowing(six_position, seven_position)
+			eight_position = calculateFollowing(seven_position, eight_position)
+			// End sol2 wackiness
+			tail_position = calculateFollowing(eight_position, tail_position)
 			tail_positions = append(tail_positions, tail_position)
+			// fmt.Println("Positions", head_position,
+			// 	one_position, two_position, three_position, four_position, five_position,
+			// 	six_position, seven_position, eight_position, tail_position)
 		}
+		// fmt.Println("Positions", head_position,
+		// 	one_position, two_position, three_position, four_position, five_position,
+		// 	six_position, seven_position, eight_position, tail_position)
 	}
 	//fmt.Println(tail_positions)
-	fmt.Println("Solution One:", len(deduplicateSlice(tail_positions)))
-
+	fmt.Println("Solution:", len(deduplicateSlice(tail_positions)))
 }
