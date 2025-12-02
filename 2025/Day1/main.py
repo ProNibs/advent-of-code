1753ad503f6c57747832b3871c3c5ed8164cdb36
Andrew Graham <andrewsgraham1995@gmail.com>
2025-12-01 21:53:08 -0600

    Solved Part1 of Day1 with some TDD and Python.

--- 2025/Day1/main.py
+++ 2025/Day1/main.py
@@ -1,2 +1,33 @@
+def shift(input_number: int, movement_string: str):
+    if movement_string.startswith('R'):
+        output = input_number + int(movement_string[1:])
+        if output >= 100:
+            output %= 100
+    elif movement_string.startswith('L'):
+        output = input_number - int(movement_string[1:])
+        if output < 0:
+            output %= 100
+    else:
+        raise ValueError
+    return output
 
-print("hello")
+def readFile(input_file: str):
+    raw_list = []
+    with open(input_file) as f:
+        raw_list = f.read().splitlines()
+    return raw_list
+
+def rotation(input_list):
+    dial_number = 50
+    times_hit_0 = 0
+    for i in input_list:
+        dial_number = shift(dial_number, i)
+        if dial_number == 0:
+            times_hit_0 += 1
+    return dial_number, times_hit_0
+
+if __name__ == '__main__':
+    print(readFile('testdata.txt'))
+    print(rotation(readFile('testdata.txt')))
+    # print(readFile('data.txt'))
+    print(rotation(readFile('data.txt')))
--- 2025/Day1/test_main.py
+++ 2025/Day1/test_main.py
@@ -1,10 +1,30 @@
 import unittest
-from Day1 import main
+from Day1.main import *
 
 class TestMain(unittest.TestCase):
 
-    def test_right():
-        self.assertEqual(right_shift(50, 'R3'), 53)
+    def test_right(self):
+        self.assertEqual(shift(50, 'R3'), 53)
+
+    def test_left(self):
+        self.assertEqual(shift(50, 'L3'), 47)
     
+    def test_loop_right(self):
+        self.assertEqual(shift(90, 'R10'), 0)
+        self.assertEqual(shift(90, 'R11'), 1)
+
+    def test_loop_left(self):
+        self.assertEqual(shift(10, 'L10'), 0)
+        self.assertEqual(shift(10, 'L11'), 99)
+
+    # Test to see if we can do all the rotations proper
+    def test_rotations(self):
+        test_data = readFile('D:/Documents/GitHub/advent-of-code/2025/Day1/testdata.txt')
+        self.assertEqual(rotation(test_data), (32, 3))
+
+    # Noticed test data had some R203 craziness
+    def test_super_rotations(self):
+        self.assertEqual(shift(80, 'R203'), 83)
+
 if __name__ == '__main__':
     unittest.main()
