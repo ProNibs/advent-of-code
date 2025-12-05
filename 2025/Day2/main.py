38e7ea655d315fce679ea03ca06a8672290e9557
Andrew Graham <andrewsgraham1995@gmail.com>
2025-12-05 17:45:32 -0600

    Finished Part 2 for Day 2. Lazy hard-coding just enough prime numbers, hehe.

--- 2025/Day2/main.py
+++ 2025/Day2/main.py
@@ -5,7 +5,7 @@ def readFile(input_file: str):
         raw_list = f.read().split(',')
     return raw_list
 
-def check_for_repeats(input: str):
+def check_for_repeats_part_one(input: str):
     output = []
     split_them = input.split('-')
     for i in range(int(split_them[0]), int(split_them[1])+1):
@@ -17,16 +17,89 @@ def check_for_repeats(input: str):
             output.append(i)
     return output
 
+
+def check_for_repeats_part_two(input: str):
+    output = []
+    split_them = input.split('-')
+    for i in range(int(split_them[0]), int(split_them[1])+1):
+        string_version = str(i)
+        if len(string_version) % 2 == 0:
+            chunked_array = chunking(string_version, 2)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        # From here on out, prime numbers matter? We only get so long though
+        if len(string_version) % 3 == 0:
+            # print(string_version)
+            chunked_array = chunking(string_version, 3)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        if len(string_version) % 5 == 0:
+            chunked_array = chunking(string_version, 5)
+            # print('Chunk array', chunked_array)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        if len(string_version) % 7 == 0:
+            chunked_array = chunking(string_version, 7)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        if len(string_version) % 11 == 0:
+            chunked_array = chunking(string_version, 11)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        if len(string_version) % 13 == 0:
+            chunked_array = chunking(string_version, 13)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+        # # More than 17 digits is...extreme
+        if len(string_version) % 17 == 0:
+            chunked_array = chunking(string_version, 17)
+            if check_chunked_arrays(chunked_array):
+                output.append(i)
+                continue
+    return output
+
+def chunking(input: list[str], chunk_number: int):
+    chunk_size = len(input) // chunk_number
+    chunked_list = []
+    # print('cHUNK', chunk_size, input)
+    for i in range(0,len(input), chunk_size):
+        chunked_list.append(int(input[i: i+chunk_size]))
+    return chunked_list
+
+def check_chunked_arrays(input: list[int]):
+    # Loop through all the arrays and return true if they all match
+    for i in input:
+        if input[0] != i:
+            return False
+    return True
+
 def solution_one(input):
     solution = 0
     for i in input:
-        repeat_numbers = check_for_repeats(i)
+        repeat_numbers = check_for_repeats_part_one(i)
         for j in repeat_numbers: 
             solution += j
     return solution
 
+def solution_two(input):
+    solution = 0
+    for i in input:
+        repeat_numbers = check_for_repeats_part_two(i)
+        for j in repeat_numbers: 
+            solution += j
+    return solution
+
+
 if __name__ == '__main__':
-    check_for_repeats('11-22')
+    check_for_repeats_part_one('11-22')
     # print(type(readFile('testdata.txt')))
     # print(solution_one(readFile('testdata.txt')))
-    print(solution_one(readFile('data.txt')))
+    # print(solution_one(readFile('data.txt')))
+    print(solution_two(readFile('data.txt')))
+    # print(check_for_repeats_part_two('2121212118-2121212124'))
--- 2025/Day2/test_main.py
+++ 2025/Day2/test_main.py
@@ -4,29 +4,74 @@ from Day2.main import *
 class TestMain(unittest.TestCase):
 
     def test_single_digit_repeats(self):
-        self.assertEqual(check_for_repeats('54-55'), [55])
+        self.assertEqual(check_for_repeats_part_one('54-55'), [55])
 
     def test_double_digit_repeats(self):
-        self.assertEqual(check_for_repeats('6463-6466'), [6464])
+        self.assertEqual(check_for_repeats_part_one('6463-6466'), [6464])
     def test_triple_digit_repeats(self):
-        self.assertEqual(check_for_repeats('123122-123125'), [123123])
+        self.assertEqual(check_for_repeats_part_one('123122-123125'), [123123])
     
     def test_based_on_example(self):
-        self.assertEqual(check_for_repeats('11-22'), [11,22])
-        self.assertEqual(check_for_repeats('95-115'), [99])
-        self.assertEqual(check_for_repeats('998-1012'), [1010])
-        self.assertEqual(check_for_repeats('1188511880-1188511890'), [1188511885])
-        self.assertEqual(check_for_repeats('222220-222224'), [222222])
-        self.assertEqual(check_for_repeats('1698522-1698528'), [])
-        self.assertEqual(check_for_repeats('446443-446449'), [446446])
-        self.assertEqual(check_for_repeats('38593856-38593862'), [38593859])
-        self.assertEqual(check_for_repeats('565653-565659'), [])
-        self.assertEqual(check_for_repeats('824824821-824824827'), [])
-        self.assertEqual(check_for_repeats('2121212118-2121212124'), [])
+        self.assertEqual(check_for_repeats_part_one('11-22'), [11,22])
+        self.assertEqual(check_for_repeats_part_one('95-115'), [99])
+        self.assertEqual(check_for_repeats_part_one('998-1012'), [1010])
+        self.assertEqual(check_for_repeats_part_one('1188511880-1188511890'), [1188511885])
+        self.assertEqual(check_for_repeats_part_one('222220-222224'), [222222])
+        self.assertEqual(check_for_repeats_part_one('1698522-1698528'), [])
+        self.assertEqual(check_for_repeats_part_one('446443-446449'), [446446])
+        self.assertEqual(check_for_repeats_part_one('38593856-38593862'), [38593859])
+        self.assertEqual(check_for_repeats_part_one('565653-565659'), [])
+        self.assertEqual(check_for_repeats_part_one('824824821-824824827'), [])
+        self.assertEqual(check_for_repeats_part_one('2121212118-2121212124'), [])
         
+    # Part 2 is...different.
+    def test_single_digit_repeats_part_two(self):
+        self.assertEqual(check_for_repeats_part_two('54-55'), [55])
+    def test_double_digit_repeats_part_two(self):
+        self.assertEqual(check_for_repeats_part_two('6463-6466'), [6464])
+    def test_triple_digit_repeats_part_two(self):
+        self.assertEqual(check_for_repeats_part_two('123122-123125'), [123123])
+    
+    def test_chunking(self):
+        self.assertEqual(chunking('11',2), [1,1])
+        self.assertEqual(chunking('111',3), [1,1,1])
+        self.assertEqual(chunking('1111',2), [11,11])
+        self.assertEqual(chunking('1111',4), [1,1,1,1])
+        self.assertEqual(chunking('1234',2), [12,34])
+        self.assertEqual(chunking('1234',4), [1,2,3,4])
+        self.assertEqual(chunking('12345',5), [1,2,3,4,5])
+        # self.assertEqual(chunking('1111',3), [1,1,1])
+        self.assertEqual(chunking('192427',3), [19,24,27])
+        
+
+    def test_triple_repeating(self):
+        self.assertEqual(check_for_repeats_part_two('110-112'), [111])
+        self.assertEqual(check_for_repeats_part_two('121211-121213'), [121212])
+    
+    def test_sixtuple_repeating(self):
+        self.assertEqual(check_for_repeats_part_two('222221-222223'), [222222])
+        self.assertEqual(check_for_repeats_part_two('212120-212122'), [212121])
+    
+    def test_five_equal_repeats(self):
+        self.assertEqual(check_for_repeats_part_two('2121212118-2121212124'), [2121212121])
+
+    def test_based_on_example_part_two(self):
+        self.assertEqual(check_for_repeats_part_two('11-22'), [11,22])
+        self.assertEqual(check_for_repeats_part_two('95-115'), [99, 111])
+        self.assertEqual(check_for_repeats_part_two('998-1012'), [999, 1010])
+        self.assertEqual(check_for_repeats_part_two('1188511880-1188511890'), [1188511885])
+        self.assertEqual(check_for_repeats_part_two('222220-222224'), [222222])
+        self.assertEqual(check_for_repeats_part_two('1698522-1698528'), [])
+        self.assertEqual(check_for_repeats_part_two('446443-446449'), [446446])
+        self.assertEqual(check_for_repeats_part_two('38593856-38593862'), [38593859])
+        self.assertEqual(check_for_repeats_part_two('565653-565659'), [565656])
+        self.assertEqual(check_for_repeats_part_two('824824821-824824827'), [824824824])
+        self.assertEqual(check_for_repeats_part_two('2121212118-2121212124'), [2121212121])
+    
     def test_data_answer(self):
         test_data = readFile('D:/Documents/GitHub/advent-of-code/2025/Day2/testdata.txt')
         self.assertEqual(solution_one(test_data), 1227775554)
+        self.assertEqual(solution_two(test_data), 4174379265)
         
 if __name__ == '__main__':
     unittest.main()
