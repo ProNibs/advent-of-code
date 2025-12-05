cd96506b1a54839b62f6b2babd2669da254eb8d9
Andrew Graham <andrewsgraham1995@gmail.com>
2025-12-05 15:32:50 -0600

    Solved Part2 of Day 5. Unsure if I needed so many repeat sorts but meh.

--- 2025/Day5/main.py
+++ 2025/Day5/main.py
@@ -23,6 +23,45 @@ def isFresh(fresh_list: list[(int,int)], input: int):
             break
     return fresh
     
+def unique_fresh(input: list[(int,int)]):
+    output = []
+    for next_item in input:
+        # print(output)
+        # Seed with first item in list
+        if output == []:
+            output.append(next_item)
+            continue
+        for index,previous_item in enumerate(output):
+            # print(next_item, index, previous_item)
+            # These account for overlap
+            if (previous_item[0] <= next_item[0]) and (previous_item[1] >= next_item[1]):
+                # print('Complete overlap')
+                break    # Complete overlap, move on 
+            elif (previous_item[0] <= next_item[0] <= previous_item[1]) and (next_item[1] > previous_item[1]):
+                    # print('Left overlap')
+                    output[index] = (previous_item[0], next_item[1])
+                    break
+            elif (previous_item[0] <= next_item[1] <= previous_item[1]) and (next_item[0] < previous_item[0]):
+                    # print('Right overlap')
+                    output[index] = (next_item[0], previous_item[1])
+                    break
+            # These account for adjacent (combine them)
+            elif previous_item[0] == next_item[1]+1:
+                # print('Adjacent left overlap')
+                output[index] = (next_item[0], previous_item[1])
+                break
+            elif previous_item[1] == next_item[0]-1:
+                # print('Adjacent right overlap')
+                output[index] = (previous_item[0], next_item[1])
+                break
+        else: # Checked against everything already put in, so cannot be combined
+            output.append(next_item)
+    return output
+    
+def sorted(input: list[(int,int)]):
+    input.sort()
+    return input
+
 
 def solution_one(input: list[str]):
     fresh_items = fresh_ingredients(input)
@@ -34,9 +73,33 @@ def solution_one(input: list[str]):
             solution += 1
     return solution
 
+def solution_two(input: list[str]):
+    fresh_items = fresh_ingredients(input)
+    solution = 0
+    # Already know I cannot create a jumbo array....
+    unique_items = sorted(fresh_items)
+    unique_items = unique_fresh(fresh_items)
+    # print(unique_items)
+    # Couple of passes is enough to combine some of them that got bridged later, right?
+    unique_items = sorted(unique_items)
+    unique_items = unique_fresh(unique_items)
+    unique_items = sorted(unique_items)
+    unique_items = unique_fresh(unique_items)
+    unique_items = unique_fresh(unique_items)
+    unique_items = unique_fresh(unique_items)
+    unique_items = unique_fresh(unique_items)
+    # print(unique_items)
+    for one_item in unique_items:
+        # Assuming right item is always higher than left; +1 is for inclusive numbers
+        solution += one_item[1] - one_item[0] + 1
+    return unique_items, solution
+
 if __name__ == '__main__':
     test_file = readFile('testdata.txt')
     real_file = readFile('data.txt')
-    print(solution_one(test_file))
-    print(solution_one(real_file))
+    # print(solution_one(test_file))
+    # print(solution_one(real_file))
+    # print(unique_fresh([(3, 5), (10, 18), (12, 20)]))
+    print(solution_two(test_file))
+    print(solution_two(real_file))
 
--- 2025/Day5/test_main.py
+++ 2025/Day5/test_main.py
@@ -5,7 +5,6 @@ test_data = readFile('D:/Documents/GitHub/advent-of-code/2025/Day5/testdata.txt'
 
 class TestMain(unittest.TestCase):
 
-
     def test_fresh_ingredient_list(self):
         # self.assertEqual(fresh_ingredients(test_data), [3,4,5,10,11,12,13,14,15,16,17,18,19,20])
         self.assertEqual(fresh_ingredients(test_data), [(3,5), (10,14), (16,20),(12,18)])
@@ -18,8 +17,39 @@ class TestMain(unittest.TestCase):
         self.assertEqual(isFresh(fresh_list, 5), True)
         self.assertEqual(isFresh(fresh_list, 6), False)
         
+    def test_unique_fresh_list_overlap(self):
+        self.assertEqual(unique_fresh([(3,5), (4,6)]), [(3,6)])
+        self.assertEqual(unique_fresh([(3,5), (2,4)]), [(2,5)])
+        self.assertEqual(unique_fresh([(3, 5), (10, 18), (12, 20)]), [(3,5),(10,20)])
+ 
+
+    def test_unique_fresh_list_shared_edge(self):
+        self.assertEqual(unique_fresh([(3,5), (5,7)]), [(3,7)])
+        self.assertEqual(unique_fresh([(3,5), (1,3)]), [(1,5)])
+    
+    def test_unique_fresh_list_adjacent(self):
+        self.assertEqual(unique_fresh([(3,5), (6,7)]), [(3,7)])
+        self.assertEqual(unique_fresh([(3,5), (1,2)]), [(1,5)])
+    
+    def test_unique_fresh_list_no_overlap(self):
+        self.assertEqual(unique_fresh([(3,5), (7,8)]), [(3,5),(7,8)])
+    
+    def test_unique_fresh_list_all_overlap(self):
+        self.assertEqual(unique_fresh([(3,6), (4,5)]), [(3,6)])
+
+    def test_sort_by_lower_end(self):
+        self.assertEqual(sorted([(3,5),(10,14),(16,20),(12,18)]), [(3,5), (10,14), (12,18),(16,20)])
+
+    # def test_combine_after_first_pass(self):
+    #     self.assertEqual(unique_fresh([(3, 5), (10, 18), (16, 20)]), [(3,5), (10,20)]) 
+    
+    # def test_unique_fresh_against_test_data(self):
+    #     fresh_items = fresh_ingredients(test_data)
+    #     self.assertEqual(unique_fresh(fresh_items), [(3,5),(10,20)])
+
     def test_data_answer(self):
         self.assertEqual(solution_one(test_data), 3)
+        self.assertEqual(solution_two(test_data), ([(3,5),(10,20)],14))
         
 if __name__ == '__main__':
     unittest.main()
