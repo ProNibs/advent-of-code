d0f1548016e9502b61319fa0e7d4d63e4236afff
Andrew Graham <andrewsgraham1995@gmail.com>
2025-12-05 18:51:03 -0600

    Spent a good time ironing out tests beforehand paid dividends here.

--- 2025/Day3/main.py
+++ 2025/Day3/main.py
@@ -6,13 +6,33 @@ def readFile(input_file: str):
     return raw_list
 
 def determine_joltage(input: list[int]):
-    # -2 is because we cannot pick the very last number
+    # -1 is because we cannot pick the very last number
     largest_number = max(input[:-1])
     # Python searches left-to-right naturally, yay
     largest_index = input.index(largest_number)
     second_largest_number = max(input[largest_index+1:])
     # print(largest_number, largest_index, second_largest_number)
     return largest_number*10 + second_largest_number
+
+def determine_joltage_part_two(input: list[int]):
+    ordered_numbers = []
+    # Seed initial number
+    # -1 is because we cannot pick the very last number
+    ordered_numbers.append(max(input[:-11]))
+    # Python searches left-to-right naturally, yay
+    previous_index = input.index(ordered_numbers[0])
+    for i in range(11):
+        print('Previous index', previous_index, 'Current iteration', i)
+        # print(max(input[previous_index+1:-1*(11-i)]))
+        if i == 10:
+            # On last iteration, the 10-i turns into 0 which is not good
+            ordered_numbers.append(max(input[previous_index+1:]))
+        else:
+            ordered_numbers.append(max(input[previous_index+1:-1*(10-i)]))
+        previous_index = input.index(ordered_numbers[i+1], previous_index+1)
+    print(ordered_numbers)
+    return int("".join(str(num) for num in ordered_numbers))
+
 
 def solution_one(input):
     solution = 0
@@ -22,11 +42,14 @@ def solution_one(input):
 
 def solution_two(input):
     solution = 0
+    for battery in input:
+        solution += determine_joltage_part_two([int(i) for i in battery])
     return solution
 
 
 if __name__ == '__main__':
     # determine_joltage([3,4,1,4])
     # print(solution_one(readFile('testdata.txt')))
-    print(solution_one(readFile('data.txt')))
-    # print(solution_two(readFile('data.txt')))
+    # print(solution_one(readFile('data.txt')))
+    print(solution_two(readFile('data.txt')))
+    # determine_joltage_part_two([9,8,7,6,5,4,3,2,1,9,1,1,1,1,9])
--- 2025/Day3/test_main.py
+++ 2025/Day3/test_main.py
@@ -9,17 +9,41 @@ class TestMain(unittest.TestCase):
 
     def test_ignore_last_digi_joltage_number(self):
         self.assertEqual(determine_joltage([3,2,1,4]), 34)
-        self.assertEqual(determine_joltage([3,4,1,4]), 34)
+        self.assertEqual(determine_joltage([3,4,1,4]), 44)
 
     def test_test_data_joltage_number(self):
         self.assertEqual(determine_joltage([9,8,7,6,5,4,3,2,1,1,1,1,1,1,1]), 98)
         self.assertEqual(determine_joltage([8,1,1,1,1,1,1,1,1,1,1,1,1,1,9]), 89)
         self.assertEqual(determine_joltage([2,3,4,2,3,4,2,3,4,2,3,4,2,7,8]), 78)
         self.assertEqual(determine_joltage([8,1,8,1,8,1,9,1,1,1,1,2,1,1,1]), 92)
+
+    # Part Two stuff
+    def test_easy_joltage_number_part_two(self):
+        self.assertEqual(len(str(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,1,1,1]))), 12)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,1,1,1]), 987654321111)
         
+    def test_repeats_joltage_number_part_two(self):
+        self.assertEqual(determine_joltage_part_two([9,8,9,6,5,4,3,2,1,1,1,1,1,1,1]), 996543211111)
+
+    def test_ignore_last_digi_joltage_number_part_two(self):
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,1,1,9]), 987654321119)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,1,9,1]), 987654321191)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,9,1,1]), 987654321911)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,9,1,1,9]), 987654329119)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,9,1,1,1,1]), 987654391111)
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,9,1,1,1,1,1]), 987654911111)
+
+    def test_test_data_joltage_number_part_two(self):
+        self.assertEqual(determine_joltage_part_two([9,8,7,6,5,4,3,2,1,1,1,1,1,1,1]), 987654321111)
+        self.assertEqual(determine_joltage_part_two([8,1,1,1,1,1,1,1,1,1,1,1,1,1,9]), 811111111119)
+        self.assertEqual(determine_joltage_part_two([2,3,4,2,3,4,2,3,4,2,3,4,2,7,8]), 434234234278)
+        self.assertEqual(determine_joltage_part_two([8,1,8,1,8,1,9,1,1,1,1,2,1,1,1]), 888911112111)
+        
+
+
     def test_data_answer(self):
         self.assertEqual(solution_one(test_data), 357)
-        # self.assertEqual(solution_two(test_data), 4174379265)
+        self.assertEqual(solution_two(test_data), 3121910778619)
         
 if __name__ == '__main__':
     unittest.main()
